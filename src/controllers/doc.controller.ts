9a3b322b8e9b9d9912186d3feb17b3c7350ef8d9
rknizzle <ryan@autotiv.com>
2019-07-24 00:04:13 -0400

    Added a documentation controller
            - The /docs path generates documentation based on swagger
    comments found in the controller files

---
+++ src/controllers/doc.controller.ts
@@ -0,0 +1,49 @@
+import * as express from 'express'
+import Controller from '../interfaces/controller.interface'
+import swaggerJSDoc from 'swagger-jsdoc'
+import swaggerUi from 'swagger-ui-express'
+import fs from 'fs'
+
+class DocController implements Controller {
+  public path = '/docs'
+  public router = express.Router()
+  private swaggerSpec
+
+  constructor() {
+    this.initializeSwagger().then(() => {
+      // /docs will run the swaggerUI middleware and render the documentation
+      this.router.use(this.path, swaggerUi.serve, swaggerUi.setup(this.swaggerSpec))
+    })
+  }
+
+  private initializeSwagger() {
+    return new Promise((resolve, reject) => {
+      // -- setup up swagger-jsdoc --
+      const swaggerDefinition = {
+        info: {
+          title: 'Template',
+          version: '1.0.0',
+          description: 'Template Project',
+        },
+        basePath: '/',
+      }
+
+      const controllers: string[] = []
+      fs.readdir('./src/controllers', (err, files) => {
+        files.forEach(file => {
+          if (file.includes('controller') && !file.includes('doc.controller')) {
+            controllers.push(`./src/controllers/${file}`)
+          }
+        })
+        const options = {
+          swaggerDefinition,
+          apis: controllers,
+        }
+        this.swaggerSpec = swaggerJSDoc(options)
+        return resolve()
+      })
+    })
+  }
+}
+
+export default DocController
